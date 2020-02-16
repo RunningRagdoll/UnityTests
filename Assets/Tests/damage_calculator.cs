de298be1276f1e48c87ea9cd7b70c963411977fc
Adam Edlund <adam92edlund@gmail.com>
2020-02-16 17:30:28 +0100

    Added Tests folder and first unit test

---
+++ Assets/Tests/damage_calculator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class damage_calculator
+    {
+        [Test]
+        public void sets_damage_to_half_at_50_percent_mitigation()
+        {
+            int finalDamage = DamageCalculator.CalculateDamage(10, 0.5f);
+        }
+    }
+}
