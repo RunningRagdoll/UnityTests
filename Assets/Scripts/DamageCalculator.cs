b1fd0f7bb27457e914e83569c8989df674278cc3
Adam Edlund <adam92edlund@gmail.com>
2020-02-16 17:29:10 +0100

    Added Scripts folder and assembly def

---
+++ Assets/Scripts/DamageCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DamageCalculator
+{
+    public static int CalculateDamage(int amount, float mitigationPercent)
+    {
+        throw new NotImplementedException();
+    }
+}
